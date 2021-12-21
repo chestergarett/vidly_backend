51ab08cb204bf453282d60d9f3f80f5880469ee6
chestergarett <chestergarett@gmail.com>
2021-12-21 21:01:59 +0800

    users model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const express = require('express');
 const app = express();
 
@@ -15,6 +16,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 //use database
 mongoose.connect('mongodb://localhost/vidly')
