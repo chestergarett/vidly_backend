19d0ef52445bcf08da5fdb13ac5a10749cd98b94
chestergarett <chestergarett@gmail.com>
2021-12-21 17:25:07 +0800

    adding transactions

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+
 //use routes
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+
 //use database
 mongoose.connect('mongodb://localhost/vidly')
     .then( () => console.log('Connected to MongoDB') )
---
+++ routes/rentals.js
@@ -0,0 +1,60 @@
+const { Rental, validate } = require('../models/rental');
+const { Movie } = require('../models/movie');
+const { Customer } = require('../models/customer');
+const Fawn = require('fawn');
+const mongoose = require('mongoose');
+const express = require('express');
+
+const router = express.Router();
+
+Fawn.init('mongodb://127.0.0.1:27017/vidly');
+
+//endpoints
+router.get('/', async(req,res) => {
+    const rentals = await Rental.find().sort('-dateOut');
+    res.send(rentals);
+});
+
+router.post('/', async(req,res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findById(req.body.customerId);
+    if (!customer) return res.status(400).send('Invalid customer.');
+
+    const movie = await Movie.findById(req.body.movieId);
+    if (!movie) return res.status(400).send('Invalid movie.');
+
+    if (movie.numberInStock === 0) return res.status(400).send('Movie out of stock.');
+
+    let rental = new Rental({
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            phone: customer.phone
+        },
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate
+        }
+    });
+    
+    //two-way transactions
+    try {
+        new Fawn.Task()
+        .save('rentals', rental)
+        .update('movies', { _id: movie._id }, { 
+            $inc: { numberInStock: -1 },
+        })
+        .run();    
+    }
+    catch(ex) {
+        res.status(500).send('Something went wrong.');
+    }
+    
+
+    res.send(rental);
+})
+
+module.exports = router;
