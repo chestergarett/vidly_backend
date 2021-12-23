809a90d47c464541f6dbd604cc9736b915ae86dd
chestergarett <chestergarett@gmail.com>
2021-12-23 15:59:17 +0800

    adding mongodb

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,10 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function(){
     //use database
-    mongoose.connect('mongodb://localhost/vidly')
+    mongoose.connect(config.get('vidly_db'))
     .then( () => winston.info('Connected to MongoDB') )
     // .catch( err => console.error('Could not connect to MongoDB'))
 }
--- startup/logging.js
+++ startup/logging.js
@@ -1,17 +1,18 @@
 
 require('express-async-errors');
 const winston = require('winston');
+const config = require('config');
 require('winston-mongodb');
 
 module.exports = function(){
     winston.add(winston.transports.File, { filename: 'logfile.log' });
     winston.add(winston.transports.MongoDB, { 
-        db: 'mongodb://localhost/vidly',
+        db: config.get('vidly_db'),
         level: 'error'
     });
 
     winston.handleExceptions(
-        new winston.transports.Console({ colorize: true, prettyPrint: true}),
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
         new winston.transports.File({ filename: 'uncaughtExceptions.log'}));
 
     process.on('unhandledRejection', (ex)=> {
